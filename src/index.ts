2ceb309e93450d35b57df545ec064c93b75c76ff
eric-yates <hello@ericthecurious.com>
2024-10-05 16:01:51 -0600

    minor: export classes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-//exports go here
+export { default as BleScannerImpl } from './BleScanner'
+export * from './BleScanner'
+
+export { default as SpyBleScanner } from './testDoubles/SpyBleScanner'
+export * from './testDoubles/SpyBleScanner'
+
+export { default as FakeNoble } from './testDoubles/FakeNoble'
+export * from './testDoubles/FakeNoble'
+
+export { default as FakeBleScanner } from './testDoubles/FakeBleScanner'
+export * from './testDoubles/FakeBleScanner'
---
+++ src/testDoubles/FakeBleScanner.ts
@@ -0,0 +1,42 @@
+import { generateId } from '@sprucelabs/test-utils'
+import { Peripheral } from '@abandonware/noble'
+import { BleScanner } from '../BleScanner'
+
+export default class FakeBleScanner implements BleScanner {
+    public callsToScanForPeripherals: string[][] = []
+
+    private fakedPeripherals: Peripheral[] = []
+
+    public async scanForPeripherals(uuids: string[]) {
+        this.callsToScanForPeripherals.push(uuids)
+        return this.fakedPeripherals
+    }
+
+    public setFakedPeripherals(uuids?: string[]) {
+        this.fakedPeripherals = this.createFakePeripherals(
+            uuids ?? this.generateRandomUuids()
+        )
+    }
+
+    private generateRandomUuids(num = 1) {
+        return Array.from({ length: num }, () => generateId())
+    }
+
+    public createFakePeripherals(uuids: string[]) {
+        return uuids.map((uuid) => {
+            return this.createFakePeripheral(uuid)
+        })
+    }
+
+    public createFakePeripheral(uuid?: string) {
+        return {
+            uuid: uuid ?? generateId(),
+            advertisement: {
+                localName: generateId(),
+                manufacturerData: Buffer.from([0x01, 0x02, 0x03, 0x04]),
+            },
+            rssi: Math.random() * 100,
+            connectable: true,
+        } as Peripheral
+    }
+}
